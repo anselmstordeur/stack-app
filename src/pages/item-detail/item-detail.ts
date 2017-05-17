0ee199d7c484ba41e1bc40b3f0be83bc227c23fd
Anselm Rochus Stordeur <stordeur@campus.tu-berlin.de>
2017-05-17 16:37:50 +0200

    Add initial app files
    Add item-details page

---
+++ src/pages/item-detail/item-detail.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {item} from "../list/list";
+
+/**
+ * Generated class for the ItemDetailPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-item-detail',
+  templateUrl: 'item-detail.html',
+})
+export class ItemDetailPage {
+
+  item: item;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    this.item = navParams.get('item');
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ItemDetailPage');
+  }
+
+}
